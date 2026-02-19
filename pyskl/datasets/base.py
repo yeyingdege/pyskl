785fbd3c55ebc0e4deb0bcaac2c0e2835f7870ca
yanghong <yang_hong@a-star.edu.sg>
2026-02-19 15:29:51 +0800

    add f1 in allowed_metrics

--- pyskl/datasets/base.py
+++ pyskl/datasets/base.py
@@ -174,7 +174,7 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
                 metric_options['top_k_accuracy'], **deprecated_kwargs)
 
         metrics = metrics if isinstance(metrics, (list, tuple)) else [metrics]
-        allowed_metrics = ['top_k_accuracy', 'mean_class_accuracy', 'mean_average_precision', 'recall']
+        allowed_metrics = ['top_k_accuracy', 'mean_class_accuracy', 'mean_average_precision', 'recall', 'f1']
 
         for metric in metrics:
             if metric not in allowed_metrics:
@@ -209,9 +209,9 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
                 continue
 
             if metric == 'recall':
-                topk = metric_options.setdefault('top_k_accuracy_multilabel',
+                topk = metric_options.setdefault('compute_class_recall',
                                                  {}).setdefault(
-                                                     'topk', (1,))
+                                                 'topk', (1,))
                 if not isinstance(topk, (int, tuple)):
                     raise TypeError('topk must be int or tuple of int, '
                                     f'but got {type(topk)}')
@@ -260,8 +260,9 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
                 eval_results['overall_recall'] = result_dict['overall_recall']
                 eval_results['overall_precision'] = result_dict['overall_precision']
 
-                log_msg = print_metrics(result_dict)
+                log_msg = print_metrics(result_dict, label_map=range(self.num_classes))
                 print_log(log_msg, logger=logger)
+                continue
 
         return eval_results
 
